60a0e1cb0383ef929bf439580e842915ef54594b
thenotoriousBCK <bkott21@gmail.com>
2018-06-12 01:59:42 -0400

    word counter

---
+++ spec/ben-word-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BenWordCountView from '../lib/ben-word-count-view';
+
+describe('BenWordCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
